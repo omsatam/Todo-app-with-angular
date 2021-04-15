252a8c7cddbf208660a5e1a7f72d31514daafd21
omsatam <62635289+omsatam@users.noreply.github.com>
2021-04-15 20:57:10 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from "./my_components/about/about.component"
-import {TodosComponent} from "./my_components/todos/Todos.component"
+import {TodosComponent} from "./my_components/todos/todos.component"
 const routes: Routes = [
   {path : "",component:TodosComponent},
   {path : "about",component: AboutComponent}
